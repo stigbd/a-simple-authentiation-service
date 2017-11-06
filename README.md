deecf036826cdbb7f49aea73ff1467e92078bd2f
Stig B. Dørmænen <stigbd@gmail.com>
2017-11-06 17:02:46 +0100

    First working version
    Signup results in a new user in the database

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # a-simple-authentiation-service
 
 A small service where a user can register and authenticate. Upon succesfull authentication a jwt will be returned.
+
+Inspired by <https://ciphertrick.com/2017/01/11/token-based-authentication-node-js-using-jwt/>
+
+Use ```docker run -p 127.0.0.1:27017:27017 --name some-mongo -d mongo```
+to start mongodb
---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let express = require('express');
+let app = express();
+let morgan = require('morgan');
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+let User = require('./models/user');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'))
+
+// Set up mongodb connection
+mongoose.connect("mongodb://localhost/demo", {useMongoClient: true});
+
+
+// ===== Public Routes =====
+
+// Get root
+app.get('/', (req, res) => {
+  res.send('hello world, from a simple authentication service');
+});
+
+app.post('/signup', function(req,res){
+  let user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+  user.save(function(err, data){
+    if(err){
+      return res.json({error: true});
+    }
+    res.json({error:false})
+  });
+
+});
+
+app.listen(3000);
+console.log('Listening on localhost:3000');
